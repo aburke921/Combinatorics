907e00e16aebac7db5554c51578bc934e09306bd
EMBMiller <36905783+EMBMiller@users.noreply.github.com>
2021-12-07 12:51:00 -0500

    Add files via upload

---
+++ enumeratinggeneorder_unfinished.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec  7 12:42:58 2021
+
+@author: emmiller
+"""
+from itertools import combinations
+
+def geneorder(n):
+    nums = []
+    for i in range(1, n+1):
+        nums.append(i)
+    
