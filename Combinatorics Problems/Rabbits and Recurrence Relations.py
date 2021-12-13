0cf6fbd2fd915ebfeb1ac2e87c51c37f8556013d
cantloupe <89407043+cantloupe@users.noreply.github.com>
2021-12-12 19:03:18 -0500

    Add files via upload

--- Combinatorics Problems/Rabbits and Recurrence Relations.py
+++ Combinatorics Problems/Rabbits and Recurrence Relations.py
@@ -1,22 +1,14 @@
-
-def get_num_of_rabbit_pairs(n_months, k_rabbits_per_litter):
-
-    prev1 = 1
-    prev2 = 1
-
-    for i in range(2, n_months):
-        total_rabbit_pairs = prev1 + k_rabbits_per_litter * prev2
-        prev2 = prev1
-        prev1 = total_rabbit_pairs
-
-    return total_rabbit_pairs
-
-
-file = open('rosalind_fib.txt', 'r')
-fileLines = file.read().split(" ")
-n_months = int(fileLines[0])
-k_rabbits_per_litter = int(fileLines[1])
-
-
-total_rabbit_pairs = get_num_of_rabbit_pairs(n_months, k_rabbits_per_litter)
-print(total_rabbit_pairs)
+string=open("rosalind_fib.txt").readlines() #import Rosalind file
+split=string[0].split() #pull the first line from the file and store it in 'split'
+months=int(split[0]) #store the n value in 'months'
+littersize=int(split[1]) #store the k value in 'littersize'
+lastmonthbreeding=0
+lastmonthtotal=1
+for i in range (months-1): 
+    currentbreeding=lastmonthtotal
+    print(lastmonthtotal)
+    currenttotal=lastmonthbreeding*littersize+lastmonthtotal
+    lastmonthbreeding=currentbreeding
+    lastmonthtotal=currenttotal
+totalpairs=lastmonthtotal
+print(totalpairs) #print result
