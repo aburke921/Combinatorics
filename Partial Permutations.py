cf716c995b1dfd97394540a47228b4eec4c52f16
aburke921 <aburke@wpi.edu>
2021-12-15 11:19:29 -0500

    Fixed "Partial Permutations.py"

--- Partial Permutations.py
+++ Partial Permutations.py
@@ -6,16 +6,43 @@ Created on Tue Dec  7 11:52:42 2021
 """
 import math
 
-
 def partial_permutation(n, k):
-    factor = math.factorial(n)
-    nminkkfact = math.factorial(n - k)
-    kfact = math.factorial(k)
 
-    denominator = kfact * nminkkfact
-    return (factor / nminkkfact)
+    return calculatePartialFactorial(n-k, n)
+
+def calculatePartialFactorial(start, stop):
+
+    factorial = 1
+    for val in range(start + 1, stop + 1):
+        # print(val)
+        factorial = factorial * val
+
+    return factorial
+
+'''
+    EXAMPLE:
+    
+                  21!         21 * 20 * 19 * 18 * 17 * 16 * 15 * 14 * 13 * 12 * 11 * 10 * 9 * 8 * 7 * 6 * 5 * 4 * 3 * 2 * 1
+    p(21, 7) =  -------   =   ----------------------------------------------------------------------------------------------
+               (21 - 7)!                                         14 * 13 * 12 * 11 * 10 * 9 * 8 * 7 * 6 * 5 * 4 * 3 * 2 * 1
+                                                       
+              
+    p(21, 7) =  21 * 20 * 19 * 18 * 17 * 16 * 15 
+'''
 
 
-print(partial_permutation(21, 7) % 1000000)
+
+
+# def partial_permutation(n, k):
+#     factor = math.factorial(n)
+#     nminkkfact = math.factorial(n - k)
+#     kfact = math.factorial(k)
+#
+#     denominator = kfact * nminkkfact
+#     return (factor / nminkkfact)
+
+
+print(partial_permutation(96, 9) % 1000000)
+
 
  
